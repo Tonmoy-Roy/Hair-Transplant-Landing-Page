c801f3a9f4b7761c1e440a81971e3f2d2f839be5
Tonmoy-Roy <roytonmoy901@gmail.com>
2025-12-02 12:07:05 +0600

    marquee bug fixed

--- app/Components/Sections/SecondSection.jsx
+++ app/Components/Sections/SecondSection.jsx
@@ -5,40 +5,35 @@ import { motion } from "framer-motion";
 
 const SecondSection = () => {
   return (
-    <div className="overflow-hidden bg-white py-8">
+    <div className="overflow-hidden bg-white py-8  md:flex">
       <motion.div
-        className="flex whitespace-nowrap"
+        className="flex"
         animate={{ x: ["0%", "-100%"] }}
         transition={{
-          duration: 40,
+          duration: 70,
           ease: "linear",
           repeat: Infinity,
         }}
       >
-        {/* Duplicate items twice for seamless loop */}
         {[...features, ...features].map((feature, index) => (
           <div
             key={index}
-            className="flex items-center gap-4 px-10 min-w-max"
+            className="md:flex items-center gap-4 px-10 min-w-max"
           >
-            {/* Icon */}
             <div>{feature.icon}</div>
 
-            {/* Text */}
-            <div className="w-[200px] md:w-[250px] shrink-0">
+            <div className="">
               <h3 className="text-xl font-bold text-gray-900">
                 {feature.title}
               </h3>
-              <p className="text-gray-600 text-sm md:text-base line-clamp-2">
+              <p className="text-gray-600 text-sm md:text-base md:w-[11vw] w-[50vw]">
                 {feature.description}
               </p>
-
             </div>
-
           </div>
         ))}
       </motion.div>
-    </div>
+    </div >
   );
 };
 
